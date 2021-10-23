250740d6f465914a3e3d83c7cbe153a5bb80028a
YoniB1995 <yonatansamfisher@gmail.com>
2021-10-23 22:37:10 +0300

    matrix practices

--- graphs.js
+++ graphs.js
@@ -1,6 +1,6 @@
 // edges list + vertices list;
 
-const vertices = ['A','B','C','D','E'];
+// const vertices = ['A','B','C','D','E'];
 
 const edges = [
     ['A','B'],
@@ -37,11 +37,52 @@ const isConnected = (node1,node2) => {
 
 // console.log(isConnected('A','D'));
 
+const vertices = ['A','B','C','D','E'];
 
 const adjacencyMatrix = [
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0], // node A = [A,B,C,D,E] // connected to B + D
+    [1, 0, 1, 0, 0], // node B = [A,B,C,D,E] // connected to A + C
     [0, 1, 0, 1, 1],
-    [1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1], // node D = [A,B,C,D,E] // connected to A + C + E
     [0, 0, 1, 1, 0]
 ]
+
+const vertexIdxs = {
+    'A':0,
+    'B':1,
+    'C':2,
+    'D':3,
+    'E':4
+}
+
+// findCloseNodes
+const findNodesMatrix = (node) => {
+    const adjacentNodes = [];
+
+    // get the row in the matrix
+    for (let i = 0; i < vertices.length; i++) {
+        let nodeVertex = vertexIdxs[node];
+        if (adjacencyMatrix[nodeVertex][i] === 1){
+            adjacentNodes.push(vertices[i])
+        }
+        
+    }
+    // loop through the row
+    // if there is 1 , push that node
+    // otherwise skip
+
+    return adjacentNodes
+}
+
+// console.log(findNodesMatrix('A'))
+
+
+// isConnected
+const isNodesConnected = (node1 , node2) => {
+    const nodeIdx1 = vertexIdxs[node1];
+    const nodeIdx2 = vertexIdxs[node2];
+    return !!adjacencyMatrix[nodeIdx1][nodeIdx2] // returns true / false 
+
+}
+
+console.log(isNodesConnected('E','B'))
