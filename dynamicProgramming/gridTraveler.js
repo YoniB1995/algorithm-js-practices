e1ca89c08b7d8bb49f419469f90bfc2ae7cbbcab
YoniB1995 <yonatansamfisher@gmail.com>
2021-12-03 17:24:22 +0200

    memo practices intro

---
+++ dynamicProgramming/gridTraveler.js
@@ -0,0 +1,15 @@
+// how many times can you travel to the final location , use only right and down.
+// gridTraveler(1,1) => 1 way to travel.
+
+const gridTraveler = (m,n) => {
+    if(m === 1 && n === 1) return 1;
+    if(m === 0 || n === 0) return 0;
+    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+}
+
+console.log(gridTraveler(1,1)) // 1
+console.log(gridTraveler(2,3)) // 3
+console.log(gridTraveler(3,2)) // 3
+console.log(gridTraveler(3,3)) // 6
+console.log(gridTraveler(18,18)); // 2333607023 - wont load , space complexity is too complex takes time for computer.
+// solution => memo.
